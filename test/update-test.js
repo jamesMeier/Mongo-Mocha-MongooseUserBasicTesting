dd432e3de725dee82b165334680da7d71d0ace92
jamesMeier <jamesmeier94@gmail.com>
2019-09-11 14:58:28 -0400

    update with claaback eithin callbacjks

---
+++ test/update-test.js
@@ -0,0 +1,48 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+describe('Updating a record', () => {
+    let joe;
+    
+    beforeEach((done) => {
+        joe = new User({ name:'Joe'});
+        joe.save()
+        .then(() => done());
+    })
+    //callback return a promise named as operation which handles the .thens
+    function assertName(operation, done) {
+        operation
+             //look for all users without criteria, this returns an array
+            .then(() => User.find({}))
+            //users are returned
+            .then((users) => {
+                assert(users.length ===1);
+                assert(users[0].name ==='Alex');
+                //done is passed from the it statements into the arguments of assertName
+                done();
+            })
+    }
+
+    it('instance type/ set n save', (done) => {
+        // console.log(joe) set only changed in the memory not the database
+        joe.set('name', 'Alex');
+        //.save returns a promise and that promise is handed off to assertName
+        assertName(joe.save(), done);
+           
+            
+
+    })
+
+    it('model instance can update', (done) => {
+        assertName(joe.updateOne({name: 'Alex'}), done);
+    })
+    it('instance type/ set n save', () => {
+
+    })
+    it('instance type/ set n save', () => {
+
+    })
+    it('instance type/ set n save', () => {
+
+    })
+})
