252274bb808d2551c1dabb34a0838af778df7756
jamesMeier <jamesmeier94@gmail.com>
2019-09-11 14:06:59 -0400

    delete

--- test/destroy-test.js
+++ test/destroy-test.js
@@ -1,28 +1,65 @@
 const assert = require('assert')
 const User = require('../src/user')
 
-describe('deleting a user', () => {
+describe('Deleting a user', () => {
+    //declase joe as a new variable
     let joe;
+    //
     beforeEach((done) => {
         joe = new User({ name:'Joe'});
         joe.save()
         .then(() => done());
     });
     it('model instance remove', (done) => {
+        //simple instance remove
         joe.remove()
+        //checking to see if joe exists in the database
+
+            // another database query that returns another promise
             .then(() => User.findOne({ name:'Joe'}))
+            //this runs with whatever was returned from afore promise
             .then((user) => {
+                //we did not find a user
                 assert(user === null)
+                //call done
                 done()
             })
     })
-    it('class method remove', () => {
-        
+
+    it('class method remove', (done) => {
+        //User model used to delete a bunch od records 
+        User.deleteMany({name: 'Joe'})
+         // another database query that returns another promise
+         .then(() => User.findOne({ name:'Joe'}))
+         //this runs with whatever was returned from afore promise
+         .then((user) => {
+             //we did not find a user
+             assert(user === null)
+             //call done
+             done()
+         })
     })
-    it('', () => {
-        
+    it('class method findOneAndRemove', (done) => {
+      User.findOneAndDelete({name:'Joe'})
+        .then(() => User.findOne({ name:'Joe'}))
+         //this runs with whatever was returned from afore promise
+         .then((user) => {
+             //we did not find a user
+             assert(user === null)
+             //call done
+             done()
+         })
     })
-    it('', () => {
-        
+
+    it('class method findByIdAndRemove', (done) => {
+        User.findByIdAndDelete(joe._id)
+        .then(() => User.findOne({ name:'Joe'}))
+         //this runs with whatever was returned from afore promise
+         .then((user) => {
+             //we did not find a user
+             assert(user === null)
+             //call done
+             done()
+         })
     })
 })
