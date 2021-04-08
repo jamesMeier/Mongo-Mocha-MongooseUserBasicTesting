cddabf93657da2994c0d8ff440d43590718c7d4d
James Meier <jamesmeier94@gmail.com>
2021-04-08 19:54:16 -0400

    virtual test

--- test/subdocument-test.js
+++ test/subdocument-test.js
@@ -13,4 +13,34 @@ describe("Subdocuments", () => {
         done();
       });
   });
+  it("can add subdocs to existing records", (done) => {
+    const joe = new User({ name: "joe", posts: [] });
+    joe
+      .save()
+      .then(() => User.findOne({ name: "joe" }))
+      .then((user) => {
+        user.posts.push({ title: "new post" });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: "joe" }))
+      .then((user) => {
+        assert(user.posts[0].title === "new post");
+        done();
+      });
+  });
+  it("It can remove existing sub doc", (done) => {
+    const joe = new User({ name: "joe", posts: [{ title: "New Title" }] });
+    joe
+      .save()
+      .then(() => User.findOne({ name: "joe" }))
+      .then((user) => {
+        user.posts[0].remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: "joe" }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
 });
---
+++ test/virtual-type-test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const User = require("./../src/user");
+
+describe("Vistual Types", () => {
+  it("postCounts returns number of posts", (done) => {
+    const joe = new User({ name: "joe", posts: [{ title: "Post Title" }] });
+  });
+  joe
+    .save()
+    .then(() => User.findOne({ name: "joe" }))
+    .then((user) => {
+      assert(joe.postCount === 1);
+      done();
+    });
+});
